ed8f570f2beab7af199aec8bfe33773e7b9b833a
DiegoZNz <121038369@upq.edu.mx>
2023-02-20 18:43:16 -0600

    Definimos métodos del personaje

--- personaje.py
+++ personaje.py
@@ -8,4 +8,18 @@ class Personaje:
     
     #metodos del personaje
     
+    def correr(self, status):
+        if(status):
+            print("el personaje " + self.nombre + " está corriendo ")
+        else:
+            print("el personaje " + self.nombre + " se detuvo")
+            
+    def lanzarGranada(self):
+        print("Se lanzó Granada")
+    
+    def recargarArma(self,municiones):
+        cargador=5 
+        cargador=cargador+municiones
+        
+        print("El arma tiene ahora " + cargador +"balas")
     
